b437853f0a3d9f8cf06dde9af00a4796d3ff3b18
cowanch <ch.cowan@gmail.com>
2020-08-02 10:20:45 -0400

    Deal cards to players after selection

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="css-card"
+       :style="{width:widthPx, height:heightPx}">
+    <p>{{cardText}}</p>
+  </div>
+</template>
+
+<style scoped>
+.css-card {
+  border: 1px solid black;
+  border-radius: 5px;
+}
+</style>
+
+<script>
+import {cardDimensions, deck} from '@/specs/cardSpecs';
+
+export default {
+  name: 'Card',
+  props: {
+    size: {
+      type: Number,
+      default: 1
+    },
+    id: String
+  },
+  computed: {
+    cardText () {
+      return deck.suspects[this.id] || deck.weapons[this.id] || deck.rooms[this.id] || '';
+    },
+    widthPx () {
+      return `${cardDimensions.width * this.size}px`;
+    },
+    heightPx () {
+      return `${cardDimensions.height * this.size}px`;
+    }
+  }
+};
+</script>
